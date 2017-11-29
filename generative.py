932b1befc4e4ddde694ff36100d63c6f4948d69e
ishitadg <ishitadasgupta@g.harvard.edu>
2017-11-28 21:52:50 -0500

    MLP without regularization

---
+++ generative.py
@@ -0,0 +1,81 @@
+import numpy as np
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+def data_gen(ps, ls, N_trials, N_blocks, N_balls):
+
+    draws = np.empty(shape = (N_trials*N_blocks, 1))
+    liks = np.empty(shape = (N_trials*N_blocks, 1))
+    pris = np.empty(shape = (N_trials*N_blocks, 1))
+    
+    urns = np.empty(shape = (N_trials*N_blocks, 1))
+    
+    # we want to remove overall bias for urn 1 vs 0
+    # let the urn on the left be assigned index 0
+    
+    # let the color that is in excess in the urn on the left
+    # be assigned index 0
+    
+    # lik represents the fraction of col0 in urn0
+    
+    # pri is the fraction of times the left urn is chosen in one block
+    # minus the number of times the right urn is chosen
+    
+    
+    for i, (p,l) in enumerate(zip(ps, ls)):
+        l = l/N_balls
+        # the probability of "high prior" 
+        # being on left or right is the same
+        pr = np.random.choice([p, 1.0 - p])
+        urn_b = np.random.binomial(1, pr, N_trials)
+        
+        pri_b = np.cumsum(- 2*urn_b + 1)
+        pri_b[1:] = pri_b[:-1]
+        pri_b[0] = 0
+        pri_b = pri_b / (np.arange(N_trials) + 1.0)
+        
+        # probability of 
+        if l < 0.5 : l = 1.0 - l
+        draws_b = []
+        for urn in urn_b:
+            if not urn:
+                lik = l
+            else:
+                lik = 1.0-l
+            # lik will always be the higher prob
+            # we want to draw col 0 with this prob
+            draw = 1.0 - np.random.binomial(1, lik, 1)
+            draws_b.append(draw)
+            
+        draws[i*N_trials : (i+1)*N_trials, 0] = draws_b
+        urns[i*N_trials : (i+1)*N_trials, 0] = urn_b
+        liks[i*N_trials : (i+1)*N_trials, 0] = l * np.ones(N_trials)
+        pris[i*N_trials : (i+1)*N_trials, 0] = pri_b
+        
+    
+    X = np.hstack((draws, liks, pris))
+    X = torch.from_numpy(X)
+    X = X.type(torch.FloatTensor)
+    
+    Y = torch.from_numpy(urns)
+    Y = Y.type(torch.LongTensor)
+    
+    
+    return(X, Y)
+
+
+
+def assign_PL(N_balls, N_blocks, var_fac, mean_fac_prior, mean_fac_likl):
+
+    alpha_p = var_fac
+    beta_p = var_fac*mean_fac_prior
+    priors = np.round(100*np.random.beta(alpha_p, beta_p, N_blocks))/100
+    
+    alpha_l = var_fac
+    beta_l = var_fac*mean_fac_likl
+    likls = np.round(N_balls*np.random.beta(alpha_l, beta_l, N_blocks))
+    
+    return priors, likls
---
+++ main.py
@@ -0,0 +1,154 @@
+import numpy as np
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+import generative
+
+torch.manual_seed(1)
+
+class MLPClassifier(nn.Module): 
+
+    def __init__(self, num_labels, input_size, nhid):
+        super(MLPClassifier, self).__init__()
+        self.fc1 = nn.Linear(input_size, nhid)
+        self.fc2 = nn.Linear(nhid, num_labels)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.tanh(x)
+        x = self.fc2(x)
+        x = F.log_softmax(x)
+        return x
+loss_function = nn.NLLLoss()
+N_epoch = 50    
+    
+# Urn example
+N_blocks = 15
+N_trials = 20
+N_balls = 9
+testN_blocks = int(N_blocks/5)
+valN_blocks = int(N_blocks/5)
+    
+
+data = {}
+data["inf_p"] = {}
+data["uninf_p"] = {}
+models = {}
+
+var_fac = 1000
+mean_fac = 0.25
+NUM_LABELS = 2
+INPUT_SIZE = 3
+nhid = 5
+
+for cond in data:
+    data[cond]["train"] = {}
+    data[cond]["val"] = {}
+    data[cond]["test"] = {}
+    models[cond] = MLPClassifier(NUM_LABELS, INPUT_SIZE, nhid)
+    
+    # Generate data
+    
+    if (cond == "uninf_p"):
+        
+        ps, ls = generative.assign_PL(N_balls, N_blocks, var_fac, 1.0, mean_fac)
+        data[cond]["train"]["X"],  data[cond]["train"]["y"] = \
+            generative.data_gen(ps, ls, N_trials, N_blocks, N_balls)
+        
+        ps, ls = generative.assign_PL(N_balls, valN_blocks, var_fac, 1.0, mean_fac)
+        data[cond]["val"]["X"],  data[cond]["val"]["y"] = \
+            generative.data_gen(ps, ls, N_trials, valN_blocks, N_balls)        
+        
+        ps, ls = generative.assign_PL(N_balls, testN_blocks, var_fac, mean_fac, 1.0)
+        data[cond]["test"]["X"],  data[cond]["test"]["y"] = \
+            generative.data_gen(ps, ls, N_trials, testN_blocks, N_balls)
+    else:
+        
+        ps, ls = generative.assign_PL(N_balls, N_blocks, var_fac, mean_fac, 1.0)
+        data[cond]["train"]["X"],  data[cond]["train"]["y"] = \
+            generative.data_gen(ps, ls, N_trials, N_blocks, N_balls)
+        
+        ps, ls = generative.assign_PL(N_balls, valN_blocks, var_fac, mean_fac, 1.0)
+        data[cond]["val"]["X"],  data[cond]["val"]["y"] = \
+            generative.data_gen(ps, ls, N_trials, valN_blocks, N_balls)
+
+        ps, ls = generative.assign_PL(N_balls, testN_blocks, var_fac, 1.0, mean_fac)
+        data[cond]["test"]["X"],  data[cond]["test"]["y"] = \
+            generative.data_gen(ps, ls, N_trials, testN_blocks, N_balls)
+        
+    
+        
+    # train models
+    
+    optimizer = optim.SGD(models[cond].parameters(), lr=0.1)
+    
+    for epoch in range(N_epoch):
+        for x, y in zip(data[cond]["train"]["X"], data[cond]["train"]["y"]):
+           
+            models[cond].zero_grad()
+    
+            target = autograd.Variable(y)
+            log_probs = models[cond](autograd.Variable(x)).view(1,-1)
+    
+            loss = loss_function(log_probs, target)
+            loss.backward()
+            optimizer.step()
+            
+    # validate models - will come back to this for resource rationality
+
+    err_val = 0    
+    err_test = 0  
+    
+    print("\n*********************\n")
+    pred = []
+    for x, y in zip(data[cond]["val"]["X"], data[cond]["val"]["y"]):
+        log_probs = models[cond](autograd.Variable(x)).view(1,-1)
+        err_val += np.exp(log_probs.data.numpy()[0][1 - y.numpy()[0]])
+        pred.append(np.exp(log_probs.data.numpy()[0][0]))
+    
+    data[cond]["val"]["y_pred"] = np.array(pred)        
+    err_val /= N_trials*valN_blocks
+    print("Validation loss on condition ", cond, " is ", err_val)
+    
+    
+    # test models
+    pred = []
+    for x, y in zip(data[cond]["test"]["X"], data[cond]["test"]["y"]):
+        log_probs = models[cond](autograd.Variable(x)).view(1,-1)
+        err_test += np.exp(log_probs.data.numpy()[0][1 - y.numpy()[0]])
+        pred.append(np.exp(log_probs.data.numpy()[0][0]))
+        
+    data[cond]["test"]["y_pred"] = np.array(pred)
+    err_test /= N_trials*testN_blocks
+    print("Test loss on condition ", cond, " is ", err_test)
+    
+        
+    print("\n*********************\n")
+
+
+
+def plot_both(dset):
+    
+    count = 0
+    fig = plt.figure(figsize=(16,8))
+    for cond in data:
+        count += 1
+        ax = fig.add_subplot(1, 2, count)
+        ax.plot(-2*data[cond][dset]["X"].numpy()[:, 0] + 1, label = "likl/ball_drawn")
+        ax.plot(data[cond][dset]["X"].numpy()[:, 2], label = "pri/N_left")
+        ax.plot(data[cond][dset]["y_pred"] - 0.5, label = "prediction")
+        ax.plot(0.5 - data[cond][dset]["y"].numpy(), label = "true", linestyle = "--")
+        ax.set_title('{0}'.format(cond))
+        ax.set_ylim([-1.4, 1.4])
+        ax.legend()
+        
+        plt.savefig('{0}{1}.png'.format(dset,round(100.0/(1.0 + mean_fac))))
+        
+        
+plot_both("val")
+plot_both("test")
+    
