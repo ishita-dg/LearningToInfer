11d6459856a11c363e6e638dc626e29857da4cf1
ishitadg <ishitadasgupta@g.harvard.edu>
2018-03-22 16:53:24 -0400

    Edits to include log-odds from LTI experiment

--- link2CP/demo.py
+++ link2CP/demo.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-N = 1000
+import json
+N = 100
 Nbin = 20
 # prior prob of choosing urn 1
 priors = {}
@@ -20,45 +21,67 @@ def ret_likl(lik_param, data):
         likls.append(d*lik_param + (1 - lik_param)*(1-d))
     return np.prod(np.array(likls))
         
-
-def plotall(priors, lik_params, likls, posts, which):
+def log_odds_func(x):
+    return(np.log(x / (1.0 - x)))
+    
+def plotall(priors, lik_params, likls, posts, which, log_odds = False):
     fig = plt.figure(figsize = (12,6))
     figtr = fig.transFigure.inverted()
+    
+    if log_odds:
+        f = log_odds_func
+        g_range = (-4, 4)
+        Xlab = "Log odds"
+    else:
+        f = lambda x : x
+        g_range = (0,1)
+        Xlab = "Probabilities"
     
     count = 1
     for cond in conds:
         ax = plt.subplot(2,4,count)
-        ax.hist(priors[cond], bins = Nbin, range = (0.0,1.0))
-        ax.set_title("Prior params".format(cond), fontsize = 15)
+        ax.hist(f(priors[cond]), bins = Nbin, normed = True, range = g_range)
+        ax.set_xlabel(Xlab)
+        ax.set_ylabel("Probability")
+        ax.set_title("Prior".format(cond), fontsize = 15)
         count += 1
         
         ax0tr = ax.transData
         xy0 = figtr.transform(ax0tr.transform((0.3,10)))
 
         ax = plt.subplot(2,4,count)
-        ax.hist(lik_params[cond], bins = Nbin, range = (0.0,1.0))
-        ax.set_title("Likelihood params".format(cond), fontsize = 15)
+        ax.hist(f(lik_params[cond]), bins = Nbin, normed = True, range = g_range)
+        ax.set_xlabel(Xlab)
+        ax.set_ylabel("Probability")
+        ax.set_title("Likelihood".format(cond), fontsize = 15)
         count += 1
         
                 
-        ax = plt.subplot(2,4,count)
-        ax.hist(likls[cond], bins = Nbin, range = (0.0,1.0))
-        ax.set_title("Likelihood (norm over H)".format(cond), fontsize = 15)
-        count += 1
+        if not log_odds:
+            ax = plt.subplot(2,4,count)
+            ax.hist(f(likls[cond]), bins = Nbin, normed = True, range = g_range)
+            ax.set_xlabel(Xlab)
+            ax.set_ylabel("Probability")
+            ax.set_title("Likelihood (norm over H)".format(cond), fontsize = 15)
+            count += 1
         
 
         ax = plt.subplot(2,4,count)
-        ax.hist(posts[cond], bins = Nbin, range = (0.0,1.0))
-        ax.set_title("Posterior probs queried".format(cond), fontsize = 15)
+        ax.hist(f(posts[cond]), bins = Nbin, normed = True, range = g_range)
+        ax.set_xlabel(Xlab)
+        ax.set_ylabel("Probability")
+        ax.set_title("Posterior probs".format(cond), fontsize = 15)
         count += 1
         ax0tr = ax.transData
         xy1 = figtr.transform(ax0tr.transform((0.3,10)))
+        
+        if log_odds: count +=1
         
     fig.tight_layout()
     
     line = matplotlib.lines.Line2D((0.744, 0.744), (0.02, 0.98), transform=fig.transFigure)
     
-    fig.lines = line,
+    if not log_odds: fig.lines = line,
     
 
     plt.savefig("type_{0}.png".format(which))
@@ -73,13 +96,21 @@ def plotall(priors, lik_params, likls, posts, which):
 which = str(3.1)
 Ndata = 1
 fac = 10.0
+mid = [0.5, 0.5, 0.4, 0.6]
+end = [0.1, 0.1, 0.2, 0.3, 0.9, 0.9, 0.8, 0.7]
+
+priors['inf'] = np.random.choice(mid, N)
+priors['uninf'] = np.random.choice(end, N)
+
+lik_params['inf'] = np.random.choice(end, N)
+lik_params['uninf'] = np.random.choice(mid, N)
 
 
-priors['inf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
-priors['uninf'] = np.clip(np.random.beta(1.0/fac, 1.0/fac, N), 0.001, 0.999)
+#priors['inf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
+#priors['uninf'] = np.clip(np.random.beta(1.0/fac, 1.0/fac, N), 0.001, 0.999)
 
-lik_params['inf'] = np.clip(np.random.beta(1.0/fac, 1.0/fac, N), 0.001, 0.999)
-lik_params['uninf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
+#lik_params['inf'] = np.clip(np.random.beta(1.0/fac, 1.0/fac, N), 0.001, 0.999)
+#lik_params['uninf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
 
 data = []
 
@@ -105,123 +136,130 @@ for cond in conds:
     
     
     
-plotall(priors, lik_params, likls, posts, which)
+plotall(priors, lik_params, likls, posts, which, log_odds = True)
+#Store log odds
+for name, t in zip(('priors', 'likls', 'posts'), (priors, lik_params, posts)):
+    for key in t:
+        t[key] = list(log_odds_func(t[key]))
+        
+    with open(name + '.txt', 'w') as fn:
+        json.dump(t,fn)
+        
+        
+##********************************************
+##3.2 : Vary only diagnosticity of likelihood + 
+##      certainty of hypotheses queried
+##********************************************
+
+#which = str(3.2)
+#Ndata = 8
+#Ndiff = 4
+
+##fac = 2
+
+#priors['inf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
+##priors['uninf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
+#priors['uninf'] = np.clip(np.random.beta(0.5, 0.5, N), 0.001, 0.999)
+
+#lik_params['inf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
+#lik_params['uninf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
 
 
-#********************************************
-#3.2 : Vary only diagnosticity of likelihood + 
-#      certainty of hypotheses queried
-#********************************************
+#data = []
 
-which = str(3.2)
-Ndata = 8
-Ndiff = 4
-
-#fac = 2
-
-priors['inf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
-#priors['uninf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
-priors['uninf'] = np.clip(np.random.beta(0.5, 0.5, N), 0.001, 0.999)
-
-lik_params['inf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
-lik_params['uninf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.001, 0.999)
-
-
-data = []
-
-for cond in conds:
-    data = []
-    for p, l in zip(priors[cond], lik_params[cond]):
-        urn = np.random.binomial(1,p)
-        if urn:
-            lik = l
-        else:
-            lik = 1 - l
+#for cond in conds:
+    #data = []
+    #for p, l in zip(priors[cond], lik_params[cond]):
+        #urn = np.random.binomial(1,p)
+        #if urn:
+            #lik = l
+        #else:
+            #lik = 1 - l
             
-        if cond == 'uninf':
-            N1s = int(Ndata/2)
-        elif cond == 'inf':
-            N1s = int(Ndata/2) + (
-                (lik > 0.5)*Ndiff - (lik < 0.5)*Ndiff)/2
+        #if cond == 'uninf':
+            #N1s = int(Ndata/2)
+        #elif cond == 'inf':
+            #N1s = int(Ndata/2) + (
+                #(lik > 0.5)*Ndiff - (lik < 0.5)*Ndiff)/2
 
-        N0s = Ndata - N1s
+        #N0s = Ndata - N1s
         
 
-        manipulated_data = np.array([0]*N0s + [1]*N1s)
-        np.random.shuffle(manipulated_data)
-        data.append(manipulated_data)
+        #manipulated_data = np.array([0]*N0s + [1]*N1s)
+        #np.random.shuffle(manipulated_data)
+        #data.append(manipulated_data)
         
         
-    data = np.array(data)
+    #data = np.array(data)
             
-    likls[cond] = np.array([ret_likl(lp, d) for lp, d in zip(lik_params[cond], data)])
+    #likls[cond] = np.array([ret_likl(lp, d) for lp, d in zip(lik_params[cond], data)])
     
-    likls_other = np.array([ret_likl(lp, d) for lp, d in zip(1 - lik_params[cond], data)])
-    posts[cond] = likls[cond] * priors[cond] / (likls[cond] * priors[cond] + likls_other * (1 - priors[cond]))
+    #likls_other = np.array([ret_likl(lp, d) for lp, d in zip(1 - lik_params[cond], data)])
+    #posts[cond] = likls[cond] * priors[cond] / (likls[cond] * priors[cond] + likls_other * (1 - priors[cond]))
     
-    likls[cond] = likls[cond] / (likls[cond] + likls_other)
+    #likls[cond] = likls[cond] / (likls[cond] + likls_other)
     
     
-plotall(priors, lik_params, likls, posts, which)
+#plotall(priors, lik_params, likls, posts, which)
 
-#********************************************
-#3.3 : Vary only parameters OR only parameters 
-#      + certainty of hypotheses queried
-#********************************************
-
-
-which = str(3.3)
-Ndata = 10
-odds = 3
-
-fac = 10
-
-priors['inf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
-priors['uninf'] = np.clip(np.random.beta(1.0/fac, 1.0/fac, N), 0.001, 0.999)
-#priors['uninf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
-
-lik_params['inf'] = np.clip(np.random.beta(0.5, 0.5, N), 0.1, 0.9)
-lik_params['uninf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.1, 0.9)
+##********************************************
+##3.3 : Vary only parameters OR only parameters 
+##      + certainty of hypotheses queried
+##********************************************
 
 
-data = []
+#which = str(3.3)
+#Ndata = 10
+#odds = 3
 
-for cond in conds:
-    data = []
-    for p, l in zip(priors[cond], lik_params[cond]):
-        urn = np.random.binomial(1,p)
-        if urn:
-            lik = l
-        else:
-            lik = 1 - l
+#fac = 10
+
+#priors['inf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
+#priors['uninf'] = np.clip(np.random.beta(1.0/fac, 1.0/fac, N), 0.001, 0.999)
+##priors['uninf'] = np.clip(np.random.beta(fac, fac, N), 0.001, 0.999)
+
+#lik_params['inf'] = np.clip(np.random.beta(0.5, 0.5, N), 0.1, 0.9)
+#lik_params['uninf'] = np.clip(np.random.beta(5.0, 5.0, N), 0.1, 0.9)
+
+
+#data = []
+
+#for cond in conds:
+    #data = []
+    #for p, l in zip(priors[cond], lik_params[cond]):
+        #urn = np.random.binomial(1,p)
+        #if urn:
+            #lik = l
+        #else:
+            #lik = 1 - l
             
-        r = lik/(1.0*(1-lik))
-        Ndiff = int(np.round(np.log(odds)/np.log(r)))
-        N1s = int(Ndata/2) + (
-            (lik > 0.5)*Ndiff - (lik < 0.5)*Ndiff)/2
+        #r = lik/(1.0*(1-lik))
+        #Ndiff = int(np.round(np.log(odds)/np.log(r)))
+        #N1s = int(Ndata/2) + (
+            #(lik > 0.5)*Ndiff - (lik < 0.5)*Ndiff)/2
 
-        N0s = Ndata - N1s
+        #N0s = Ndata - N1s
         
-        #if not Ndiff:
-            #print(lik, np.log(r))
+        ##if not Ndiff:
+            ##print(lik, np.log(r))
         
 
-        manipulated_data = np.array([0]*N0s + [1]*N1s)
-        np.random.shuffle(manipulated_data)
-        data.append(manipulated_data)
+        #manipulated_data = np.array([0]*N0s + [1]*N1s)
+        #np.random.shuffle(manipulated_data)
+        #data.append(manipulated_data)
         
         
-    data = np.array(data)
+    #data = np.array(data)
             
-    likls[cond] = np.array([ret_likl(lp, d) for lp, d in zip(lik_params[cond], data)])
+    #likls[cond] = np.array([ret_likl(lp, d) for lp, d in zip(lik_params[cond], data)])
     
-    likls_other = np.array([ret_likl(lp, d) for lp, d in zip(1 - lik_params[cond], data)])
-    posts[cond] = likls[cond] * priors[cond] / (likls[cond] * priors[cond] + likls_other * (1 - priors[cond]))
+    #likls_other = np.array([ret_likl(lp, d) for lp, d in zip(1 - lik_params[cond], data)])
+    #posts[cond] = likls[cond] * priors[cond] / (likls[cond] * priors[cond] + likls_other * (1 - priors[cond]))
     
-    likls[cond] = likls[cond] / (likls[cond] + likls_other)
+    #likls[cond] = likls[cond] / (likls[cond] + likls_other)
     
     
-plotall(priors, lik_params, likls, posts, which)
+#plotall(priors, lik_params, likls, posts, which)
 
 
 
