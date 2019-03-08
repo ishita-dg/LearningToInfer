1a38ea051691d2c24ec2d44812df44637d5dcb9e
ishitadg <ishitadasgupta@g.harvard.edu>
2019-03-08 17:04:25 -0500

    System neglect for new experiment

--- data/EU_descriptive_analyses.R
+++ data/EU_descriptive_analyses.R
@@ -1,4 +1,7 @@
-library("rjson")
+library(jsonlite)
+library(ggplot2)
+require(gridExtra)
+
 setwd("~/GitHub/LearningToInfer/data")
 
 log_odds <- function(x){
@@ -12,7 +15,7 @@ for (f in files){
     fn <- f
   }
 }
-EU_data <- fromJSON(file=fn)
+EU_data <- fromJSON(txt=fn)
 
 # Informative data
 lik_los <- log_odds(EU_data$ID_liks)
@@ -80,3 +83,71 @@ UD_plot_lik <- ggplot(df, aes(y = posterior, x = factor(cut_lik), fill = kind)) 
 grid.arrange(ID_plot_lik, ID_plot_prior, UD_plot_lik, UD_plot_prior, layout_matrix = rbind(c(1,2),c(3,4)))
 g<- arrangeGrob(ID_plot_lik, ID_plot_prior, UD_plot_lik, UD_plot_prior, layout_matrix = rbind(c(1,2),c(3,4)))
 ggsave(file = "EU_log_odds_regression.pdf", g)
+
+
+### Check for system neglect
+all_breaks = c(0.0000000, 0.4054651, 0.6263815, 1.3862944, 2.1972246)
+
+fn = "N_part19__expt_nameEU__NHID1__NONLINrbf__L20.0__test_epoch0__test_lr0.0__train_epoch50__train_lr0.05__plot_data"
+
+data <- fromJSON(txt=fn)
+prior_los <- log_odds(data$ID_priors)
+true_los <- log_odds(data$ID_hrms)
+lik_los <- log_odds(data$ID_liks)
+model_los <- log_odds(data$ID_ams)
+
+
+# Bin the lik_los
+
+bins = .bincode(abs(lik_los), breaks = all_breaks)
+
+
+model_lm0 <- lm(model_los[bins == 1] ~ prior_los[bins == 1] + lik_los[bins == 1] + 0)
+summary(model_lm0)
+
+model_lm1 <- lm(model_los[bins == 2] ~ prior_los[bins == 2] + lik_los[bins == 2] + 0)
+summary(model_lm1)
+
+model_lm2 <- lm(model_los[bins == 3] ~ prior_los[bins == 3] + lik_los[bins == 3] + 0)
+summary(model_lm2)
+
+model_lm3 <- lm(model_los[bins == 4] ~ prior_los[bins == 4] + lik_los[bins == 4] + 0)
+summary(model_lm3)
+
+df = data.frame(prior = (all_breaks[2:5] + all_breaks[1:4])/2.0,  
+                coeff = c(model_lm0$coefficients[2], model_lm1$coefficients[2], model_lm2$coefficients[2], model_lm3$coefficients[2]))
+plot1 <- ggplot(df, aes(y = coeff, x = prior)) +
+  geom_point() + geom_line() + ggtitle("Lik binned regression for ID")
+plot1
+
+# Prior bins
+data <- fromJSON(txt=fn)
+prior_los <- log_odds(data$UD_priors)
+true_los <- log_odds(data$UD_hrms)
+lik_los <- log_odds(data$UD_liks)
+model_los <- log_odds(data$UD_ams)
+
+bins = .bincode(abs(prior_los), breaks = all_breaks)
+
+model_lm0 <- lm(model_los[bins == 1] ~ prior_los[bins == 1] + lik_los[bins == 1] + 0)
+summary(model_lm0)
+
+model_lm1 <- lm(model_los[bins == 2] ~ prior_los[bins == 2] + lik_los[bins == 2] + 0)
+summary(model_lm1)
+
+model_lm2 <- lm(model_los[bins == 3] ~ prior_los[bins == 3] + lik_los[bins == 3] + 0)
+summary(model_lm2)
+
+model_lm3 <- lm(model_los[bins == 4] ~ prior_los[bins == 4] + lik_los[bins == 4] + 0)
+summary(model_lm3)
+
+df = data.frame(prior = (all_breaks[2:5] + all_breaks[1:4])/2.0,  
+                coeff = c(model_lm0$coefficients[1], model_lm1$coefficients[1], model_lm2$coefficients[1], model_lm3$coefficients[1]))
+plot2 <- ggplot(df, aes(y = coeff, x = prior)) +
+  geom_point() + geom_line() + ggtitle("Prior binned regression for UD")
+plot2
+
+# All plots together
+grid.arrange(plot1, plot2, nrow = 2)
+g <- arrangeGrob(plot1, plot2, nrow = 2)
+ggsave(file = "EU_binned_log_odds_regression.pdf", g)
