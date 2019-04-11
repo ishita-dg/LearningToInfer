0a9aa03b7609f8dd41da8c9cd3d8031e40f2b4cc
ishitadg <ishitadasgupta@g.harvard.edu>
2019-04-10 20:56:25 -0400

    Fixed error in SE for GT

--- data/Analyse_GT.R
+++ data/Analyse_GT.R
@@ -10,14 +10,26 @@ log_odds <- function(x){
 }
 cbbPalette <- c("grey50", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
+# fn = "N_part19__alpha0.1__expt_nameGTstudy1__NHID1__NONLINrbf__L20.0__test_epoch0__test_lr0.0__train_epoch300__train_lr0.01__plot_data"
+# data1 <- as.data.frame(fromJSON(txt=fn))
+# 
+# fn = "N_part19__alpha0.01__expt_nameGTstudy1__NHID1__NONLINrbf__L20.0__test_epoch0__test_lr0.0__train_epoch300__train_lr0.01__plot_data"
+# data2 <- as.data.frame(fromJSON(txt=fn))
+# 
+# fn = "N_part19__expt_nameGTstudy1__NHID1__NONLINrbf__L20.0__test_epoch0__test_lr0.0__train_epoch300__train_lr0.01__plot_data"
+# data3 <- as.data.frame(fromJSON(txt=fn))
+# 
+# data0 <- rbind(data1, data2)
+# data <- rbind(data3, data0)
+
 fn = "N_part19__alpha0.01__expt_nameGTstudy1__NHID1__NONLINrbf__L20.0__test_epoch0__test_lr0.0__train_epoch300__train_lr0.01__plot_data"
-data <- fromJSON(txt=fn)
+data <- as.data.frame(fromJSON(txt=fn))
 
 true_llo = log(log_odds(data$hrms))
 model_llo = log(log_odds(data$ams))
 strength_l = log(abs(data$strength))
 weight_l = log(data$weight*33)
-exact = rep(c(rep(TRUE, 300), rep(FALSE, 0)),20)
+exact = rep(c(rep(TRUE, 300), rep(FALSE, 300)),54)
 
 model_lm0 <- lm(true_llo - log(log(0.6/0.4)) ~ strength_l + weight_l + 0)
 summary(model_lm0)
@@ -28,8 +40,8 @@ summary(model_lm_random)
 model_lm_stimuli <- lm(model_llo[exact] - log(log(0.6/0.4)) ~ strength_l[exact] + weight_l[exact] + 0)
 summary(model_lm_stimuli)
 
-df = data.frame(mu = summary(model_lm_stimuli)$coefficients[1:2],
-  se = summary(model_lm_stimuli)$coefficients[2:3],
+df = data.frame(mu = summary(model_lm_stimuli)$coefficients[1:2,1],
+  se = summary(model_lm_stimuli)$coefficients[1:2,2],
   which = factor(c('Strength', 'Weight'))
 )
 
@@ -37,6 +49,7 @@ df$participants <- c(0.88, 0.31)
 
 part <- aes(x = which, y = df$participants)
 
+limits <- aes(ymax = mu + se, ymin=mu - se)
 
 p1 <- ggplot(df, aes(y=mu, x=which)) + 
   #bars
@@ -44,7 +57,7 @@ p1 <- ggplot(df, aes(y=mu, x=which)) +
   geom_point(mapping = aes(x = which, y = participants), position = 'dodge')+
   #0 to 1
   #golden ratio error bars
-  # geom_errorbar(limits, position="dodge", width=0.31)+
+  geom_errorbar(limits, position="dodge", width=0.31)+
   # #point size
   # geom_point(size=3)+
   scale_fill_manual(values=cbbPalette[c(6,7)])+
