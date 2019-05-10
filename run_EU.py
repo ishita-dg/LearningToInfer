dbf7696808baa41f885ab10b180c738b08d5aacd
ishitadg <ishitadasgupta@g.harvard.edu>
2019-05-10 18:27:28 -0400

    Edited test training tro Urn experiment

--- run_EU.py
+++ run_EU.py
@@ -34,7 +34,7 @@ for part_number in np.arange(total_part):
   # Modify in the future to read in / sysarg
   config = {'N_part' : part_number,
             'optimization_params': {'train_epoch': 50,
-                                   'test_epoch': 5,
+                                   'test_epoch': 1,
                                    'L2': 0.0,
                                    'train_lr': 0.05,
                                    'test_lr' : 0.05},
